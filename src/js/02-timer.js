11345659aae763533accddbf2104db35b665a015
AntoninaHN <87104852+AntoninaHN@users.noreply.github.com>
2021-11-18 12:31:46 +0200

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -72,37 +72,187 @@ const options = {
 
 flatpickr(refs.inputDateTimePicker, options);
 
-refs.btnStart.addEventListener('click', onStart);
+// refs.btnStart.addEventListener('click', onStart);
 
-function onStart(e) {
-  refs.btnStart.setAttribute('disabled', '');
-  refs.inputDateTimePicker.setAttribute('disabled', '');
-  refs.timerId = setInterval(timerRun, 1000);
-}
+// function onStart(e) {
+//   refs.btnStart.setAttribute('disabled', '');
+//   refs.inputDateTimePicker.setAttribute('disabled', '');
+//   refs.timerId = setInterval(timerRun, 1000);
+// }
 
-function timerRun(e) {
-  refs.dateNowGlobal = new Date();
+// function timerRun(e) {
+//   refs.dateNowGlobal = new Date();
 
-  let deltaTimeMs = refs.selectedDate - refs.dateNowGlobal;
+//   let deltaTimeMs = refs.selectedDate - refs.dateNowGlobal;
+//   console.log(deltaTimeMs);
+//   let deltaTimeMsObj = convertToMs(deltaTimeMs);
 
-  console.log(deltaTimeMs);
-  let deltaTimeMsObj = convertToMs(deltaTimeMs);
+//   const { days, hours, minutes, seconds } = deltaTimeMsObj;
 
-  const { days, hours, minutes, seconds } = deltaTimeMsObj;
+//   refs.daysValue.textContent = addLeadingZero(days);
+//   refs.hoursValue.textContent = addLeadingZero(hours);
+//   refs.minutesValue.textContent = addLeadingZero(minutes);
+//   refs.secondsValue.textContent = addLeadingZero(seconds);
 
-  refs.daysValue.textContent = addLeadingZero(days);
-  refs.hoursValue.textContent = addLeadingZero(hours);
-  refs.minutesValue.textContent = addLeadingZero(minutes);
-  refs.secondsValue.textContent = addLeadingZero(seconds);
+//   if (deltaTimeMs < 1000) {
+//     Notify.success('Timer is Over!', {
+//       clickToClose: true,
+//       position: 'center-center',
+//     });
+//     clearInterval(refs.timerId);
+//   }
+// }
 
-  if (deltaTimeMs < 1000) {
-    Notify.success('Timer is Over!', {
-      clickToClose: true,
-      position: 'center-center',
-    });
-    clearInterval(refs.timerId);
-  }
-}
-//UNI TIME
-// const date = new Date();
-// console.log(date.getTime());
+// //OLEG S.
+// //Описан в документации
+// import flatpickr from 'flatpickr';
+// // Дополнительный импорт стилей
+// import 'flatpickr/dist/flatpickr.min.css';
+// const startBtn = document.querySelector('button[data-start]');
+// startBtn.setAttribute('disabled', 'disabled');
+
+// let inputEl = document.querySelector('#datetime-picker');
+// const dataTimePicker = document.querySelector('#datetime-picker');
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//     if (selectedDates[0] < new Date()) {
+//       window.alert('Please choose a date in the future');
+//       startBtn.setAttribute('disabled', 'disabled');
+//     } else {
+//       startBtn.removeAttribute('disabled', '');
+
+//       startBtn.addEventListener('click', onStart);
+//       //inputEl = setInterval(b, 1000);
+
+//       function onStart(e) {
+// startBtn.setAttribute('disabled', '');
+// dataTimePicker.setAttribute('disabled', '');
+// // time.setAttribute('disabled', '');
+// inputEl = setInterval(b, 1000);
+//       }
+//     }
+//     function b() {
+// let deltaTimeMs = selectedDates[0] - new Date();
+
+// console.log(deltaTimeMs);
+// let deltaTimeMsObj = convertMs(deltaTimeMs);
+
+//       const { days, hours, minutes, seconds } = convertMs(deltaTimeMs);
+//       document.querySelector('span[data-days]').textContent = days;
+//       document.querySelector('span[data-hours]').textContent = hours;
+//       document.querySelector('span[data-minutes]').textContent = minutes;
+//       document.querySelector('span[data-seconds]').textContent = seconds;
+
+//       if (deltaTimeMs < 1000) {
+//         console.log('Timer is Over!');
+//         {
+//         }
+//         clearInterval(inputEl);
+//       }
+//     }
+//   },
+// };
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+// flatpickr('input#datetime-picker', options);
+
+// // Описан в документации
+// import flatpickr from 'flatpickr';
+// // Дополнительный импорт стилей
+// import 'flatpickr/dist/flatpickr.min.css';
+// const startBtn = document.querySelector('button[data-start]');
+// startBtn.setAttribute('disabled', 'disabled');
+
+// let inputEl = document.querySelector('#datetime-picker');
+// const dataTimePicker = document.querySelector('#datetime-picker');
+
+// const options = {
+//   enableTime: true,
+//   time_24hr: true,
+//   defaultDate: new Date(),
+//   minuteIncrement: 1,
+//   onClose(selectedDates) {
+//     console.log(selectedDates[0]);
+//     if (selectedDates[0] < new Date()) {
+//       window.alert('Please choose a date in the future');
+//       startBtn.setAttribute('disabled', 'disabled');
+//     } else {
+//       startBtn.removeAttribute('disabled', '');
+
+//       startBtn.addEventListener('click', () => {
+//         startBtn.setAttribute('disabled', '');
+//         dataTimePicker.setAttribute('disabled', '');
+//         inputEl = setInterval(b, 1000);
+//       });
+//       function b() {
+//         let deltaTimeMs = selectedDates[0] - new Date();
+//         let deltaTimeMsObj = convertMs(deltaTimeMs);
+
+//         const { days, hours, minutes, seconds } = convertMs(deltaTimeMs);
+//         document.querySelector('span[data-days]').textContent = days;
+//         document.querySelector('span[data-hours]').textContent = hours;
+//         document.querySelector('span[data-minutes]').textContent = minutes;
+//         document.querySelector('span[data-seconds]').textContent = seconds;
+
+//         if (deltaTimeMs < 1000) {
+//           console.log('Timer is Over!');
+//           {
+//           }
+//           clearInterval(inputEl);
+//         }
+//       }
+//     }
+//   },
+// };
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
+
+// // console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// // console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// // console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+// flatpickr('input#datetime-picker', options);
